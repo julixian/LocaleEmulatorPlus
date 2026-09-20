21fc37a0b92a8efc3dbb4d2b1e31741a3bd7a800
julixian <tianquyesss@gmail.com>
2026-09-21 05:23:50 +0800

    Bump version to 2.0.2.0

--- LEPUpdater/Properties/AssemblyInfo.cs
+++ LEPUpdater/Properties/AssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("0bae7b36-1202-460d-8ee3-61d2aa98cf6a")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
